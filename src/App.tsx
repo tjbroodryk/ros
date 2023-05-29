a11673725c86ffff5c5a3a992dad7fc39737a2da
Tj Broodryk <tjbroodryk1+1@googlemail.com>
2023-05-29 11:28:24 +0100

    build: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import us from './assets/caz_teej.jpeg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={us} />
+      </div>
+      <h1>ROS</h1>
+    </>
+  )
+}
+
+export default App
